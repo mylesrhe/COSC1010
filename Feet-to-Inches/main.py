af30a7a736d3bee614b0523e69cbab2ab3989a71
mylesrhe <mylestollefson@westernwyoming.edu>
2025-02-23 18:22:31 +0000

    ready for grading

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -6,13 +6,13 @@
 #
 # Use comments liberally throughout the program.
 
-INCHES_PER_FOOT = 12 # Universal constant for inches per foot
+INCHES_PER_FOOT = 12 # Universal constant for inches per foot.
  
-feet = 0             # Feet variable
+feet = 0             # Feet variable.
 
 # Defines the main function.
 def main ():
-    # Asks for input from user requesting amount of feet 
+    # Asks for input from user requesting amount of feet. 
     feet = int(input ("input amount of feet "))
 
     # Runs the feet to inches function and prints out the result.
@@ -23,5 +23,5 @@ def feet_to_inches (feet):
     return feet * INCHES_PER_FOOT
 
 
-# Runs the main function
+# Runs the main function.
 main ()
