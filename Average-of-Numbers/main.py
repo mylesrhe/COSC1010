1fc0017e10835c4c569e0fb0bff370840cbb4bb1
mylesrhe <mylestollefson@westernwyoming.edu>
2025-03-16 17:42:42 +0000

    ready for graiding

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -4,22 +4,22 @@
 # Average of Numbers Programming Project
 # COSC 1010
 #
-
+# Defines the main function.
 def main ():
-# opens numbers file
+# This opens the numbers file.
     numbers = open('/workspaces/COSC1010/Average-of-Numbers/numbers.txt','r')
 
-    # stores numbers retrieved in respective variables
+    # Stores numbers retrieved in there respective variables.
     num1 = int(numbers.readline())
     num2 = int(numbers.readline())
     num3 = int(numbers.readline())
 
-    # calculates average
+    # Calculates the average.
     average = (num1+num2+num3)/3
 
-    # prints average
+    # Prints the resulting average.
     print(average)
 
-#call the main function
+# Calls the main function.
 if __name__ == '__main__':
     main()
