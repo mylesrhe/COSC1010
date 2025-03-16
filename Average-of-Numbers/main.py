f0c72cecd6e23163b9476a7cd5c59483083d8e93
mylesrhe <mylestollefson@westernwyoming.edu>
2025-03-16 17:39:13 +0000

    ready for grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,25 @@
 #
-# Name
-# Date
+# Myles Tollefson
+# 3/16/2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+def main ():
+# opens numbers file
+    numbers = open('/workspaces/COSC1010/Average-of-Numbers/numbers.txt','r')
+
+    # stores numbers retrieved in respective variables
+    num1 = int(numbers.readline())
+    num2 = int(numbers.readline())
+    num3 = int(numbers.readline())
+
+    # calculates average
+    average = (num1+num2+num3)/3
+
+    # prints average
+    print(average)
+
+#call the main function
+if __name__ == '__main__':
+    main()
