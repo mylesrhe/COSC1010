c9edda0f27466b713b4b84f47d5ec380815401e1
mylesrhe <mylestollefson@westernwyoming.edu>
2025-04-13 19:09:12 +0000

    ready for grading

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,43 @@
 #
-# Name
-# Date
+# Myles Tollefson
+# 4/13/2025
 # Pig Latin Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+# Defines main function. 
+def main():
+    # Stores an empty string that will be apended later. 
+    new_string = ('')
+
+    # Gets user input.
+    phrase = input('enter a sentence to be converted ')
+
+    # Splits the string into a list.
+    phrase_list = phrase.split()
+
+    # For loop that will iterate for every word in the list.
+    for word in phrase_list:
+    
+        # Stores the first leter from the word in the 'first' variable.
+        first = word[0:1]
+    
+        # If the word is two letters or longer
+        # the following leters are stored in the 'second' variable. 
+        if len(word) > 1:
+            second = word[1:]
+        
+            # If the word is two letters or longer, the 'second' variable is added to the 'first' 
+            # putting the first letter from the word behind all the other letters in the word and an 'ay' is added.
+            new_string += str(second + first + "ay ")
+    
+        # If the word is only one letter an 'ay' is added.
+        else:
+            new_string += str(first + "ay ")
+
+    # Prints the new string.    
+    print(new_string)
+
+
+if __name__ == '__main__':
+    main()
